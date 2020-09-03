335696cab8789406bb4db1b0b505ed31fb7d0064
hexatester <revolusi147id@gmail.com>
2020-09-03 20:09:31 +0700

    add BaseUser

--- moodlepy/core/core.py
+++ moodlepy/core/core.py
@@ -1,14 +1,19 @@
-from moodlepy import BaseMoodle, BaseMessage, BaseWebservice
+from moodlepy import BaseMoodle, BaseMessage, BaseUser, BaseWebservice
 
 
 class Core(BaseMoodle):
     def __post_init__(self, moodle) -> None:
         self._message = BaseMessage(moodle)
+        self._user = BaseUser(moodle)
         self._webservice = BaseWebservice(moodle)
 
     @property
     def message(self) -> BaseMessage:
         return self._message
+
+    @property
+    def user(self) -> BaseUser:
+        return self._user
 
     @property
     def webservice(self) -> BaseWebservice:
