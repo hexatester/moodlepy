36077edbd96ef165646beec8ee36afab632a4f57
hexatester <revolusi147id@gmail.com>
2020-09-04 21:28:57 +0700

    register bases

--- moodle/core/core.py
+++ moodle/core/core.py
@@ -1,12 +1,28 @@
-from moodle import BaseMoodle, BaseBlog, BaseMessage, BaseUser, BaseWebservice
+from moodle import (BaseMoodle, BaseBlog, BaseCalendar, BaseCohort, BaseCourse,
+                    BaseMessage, BaseUser, BaseWebservice)
 
 
 class Core(BaseMoodle):
     def __post_init__(self, moodle) -> None:
         self._blog = BaseBlog(moodle)
+        self._calendar = BaseCalendar(moodle)
+        self._cohort = BaseCohort(moodle)
+        self._course = BaseCourse(moodle)
         self._message = BaseMessage(moodle)
         self._user = BaseUser(moodle)
         self._webservice = BaseWebservice(moodle)
+
+    @property
+    def calendar(self) -> BaseCalendar:
+        return self._calendar
+
+    @property
+    def cohort(self) -> BaseCohort:
+        return self._cohort
+
+    @property
+    def course(self) -> BaseCourse:
+        return self._course
 
     @property
     def blog(self) -> BaseBlog:
