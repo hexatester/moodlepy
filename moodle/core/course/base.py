3d4d9734bb015416ee02fa3a2e83d001f6ffc11b
hexatester <revolusi147id@gmail.com>
2020-09-05 21:46:11 +0700

    prepare BaseCourse

--- moodle/core/course/base.py
+++ moodle/core/course/base.py
@@ -1,14 +1,120 @@
-from dacite import from_dict
-from typing import List
 from moodle import BaseMoodle
-from . import CourseCategory
 
 
 class BaseCourse(BaseMoodle):
-    def get_categories(self) -> List[CourseCategory]:
-        datas = self.moodle.get('core_course_get_categories')
-        results: List[CourseCategory] = []
-        if datas:
-            for data in datas:
-                results.append(from_dict(CourseCategory, data))
-        return results
+    def check_updates(self):
+        res = self.moodle.post("core_course_check_updates")
+        return res
+
+    def create_categories(self):
+        res = self.moodle.post("core_course_create_categories")
+        return res
+
+    def create_courses(self):
+        res = self.moodle.post("core_course_create_courses")
+        return res
+
+    def delete_categories(self):
+        res = self.moodle.post("core_course_delete_categories")
+        return res
+
+    def delete_courses(self):
+        res = self.moodle.post("core_course_delete_courses")
+        return res
+
+    def delete_modules(self):
+        res = self.moodle.post("core_course_delete_modules")
+        return res
+
+    def duplicate_course(self):
+        res = self.moodle.post("core_course_duplicate_course")
+        return res
+
+    def edit_module(self):
+        res = self.moodle.post("core_course_edit_module")
+        return res
+
+    def edit_section(self):
+        res = self.moodle.post("core_course_edit_section")
+        return res
+
+    def get_activities_overview(self):
+        res = self.moodle.post("core_course_get_activities_overview")
+        return res
+
+    def get_categories(self):
+        res = self.moodle.post("core_course_get_categories")
+        return res
+
+    def get_contents(self):
+        res = self.moodle.post("core_course_get_contents")
+        return res
+
+    def get_course_module(self):
+        res = self.moodle.post("core_course_get_course_module")
+        return res
+
+    def get_course_module_by_instance(self):
+        res = self.moodle.post("core_course_get_course_module_by_instance")
+        return res
+
+    def get_courses(self):
+        res = self.moodle.post("core_course_get_courses")
+        return res
+
+    def get_courses_by_field(self):
+        res = self.moodle.post("core_course_get_courses_by_field")
+        return res
+
+    def get_enrolled_courses_by_timeline_classification(self):
+        res = self.moodle.post(
+            "core_course_get_enrolled_courses_by_timeline_classification")
+        return res
+
+    def get_enrolled_users_by_cmid(self):
+        res = self.moodle.post("core_course_get_enrolled_users_by_cmid")
+        return res
+
+    def get_module(self):
+        res = self.moodle.post("core_course_get_module")
+        return res
+
+    def get_recent_courses(self):
+        res = self.moodle.post("core_course_get_recent_courses")
+        return res
+
+    def get_updates_since(self):
+        res = self.moodle.post("core_course_get_updates_since")
+        return res
+
+    def get_user_administration_options(self):
+        res = self.moodle.post("core_course_get_user_administration_options")
+        return res
+
+    def get_user_navigation_options(self):
+        res = self.moodle.post("core_course_get_user_navigation_options")
+        return res
+
+    def import_course(self):
+        res = self.moodle.post("core_course_import_course")
+        return res
+
+    def search_courses(self):
+        res = self.moodle.post("core_course_search_courses")
+        return res
+
+    def set_favourite_courses(self):
+        res = self.moodle.post("core_course_set_favourite_courses")
+        return res
+
+    def update_categories(self):
+        res = self.moodle.post("core_course_update_categories")
+        return res
+
+    def update_courses(self):
+        res = self.moodle.post("core_course_update_courses")
+        return res
+
+    def view_course(self):
+        res = self.moodle.post("core_course_view_course")
+        return res
