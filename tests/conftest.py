8025e32fe7810cd450ddb0c4afc3cb32a5802e8e
hexatester <revolusi147id@gmail.com>
2020-09-09 00:52:18 +0700

    add user_id courses

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,8 @@
 from pytest import fixture
+from typing import List
+
 from moodle import Moodle
+from moodle.core.course import Course
 
 
 @fixture
@@ -8,3 +11,14 @@ def moodle() -> Moodle:
     username = 'manager'
     password = 'moodle'
     return Moodle.login(domain, username, password)
+
+
+@fixture
+def user_id(moodle: Moodle) -> int:
+    site_info = moodle.core.webservice.get_site_info()
+    return site_info.userid
+
+
+@fixture
+def courses(moodle: Moodle) -> List[Course]:
+    return moodle.core.course.get_courses()
