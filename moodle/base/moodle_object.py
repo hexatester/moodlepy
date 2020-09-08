b2c1198fa52867f50615c5a46f54137d78edaf39
hexatester <revolusi147id@gmail.com>
2020-09-08 11:30:15 +0700

    refactor MoodleObject

--- moodle/base/moodle_object.py
+++ moodle/base/moodle_object.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-from typing import Type, TypeVar, TYPE_CHECKING
+from typing import Optional, Type, TypeVar, TYPE_CHECKING
 from moodle.utils.helper import from_dict
 if TYPE_CHECKING:
     from moodle import Moodle
@@ -8,7 +8,18 @@ T = TypeVar('T')
 
 
 class MoodleObject(object):
+    def __post_init__(self):
+        self._moodle: Optional[Moodle] = None
+
+    @property
+    def moodle(self) -> Optional[Moodle]:
+        return self._moodle
+
     @classmethod
     def from_data(cls: Type[T], data: dict, moodle: Moodle) -> T:
         self = from_dict(cls, data)
+        setattr(self, '_moodle', moodle)
         return self
+
+    def __str__(self) -> str:
+        return getattr(self, 'name', repr(self))
