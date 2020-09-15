8080774d3571a1c9dd9b130b6f00202f419ce047
hexatester <revolusi147id@gmail.com>
2020-09-16 00:02:02 +0700

    add docstring

--- moodle/mod/assign/base.py
+++ moodle/mod/assign/base.py
@@ -19,6 +19,16 @@ class BaseAssign(BaseMoodle):
             courseids: Optional[List[int]] = None,
             capabilities: Optional[List[str]] = None,
             includenotenrolledcourses: Optional[int] = None) -> Assignments:
+        """Returns the courses and assignments for the users capability
+
+        Args:
+            courseids (Optional[List[int]], optional): 0 or more course ids. Defaults to None.
+            capabilities (Optional[List[str]], optional): list of capabilities used to filter courses. Defaults to None.
+            includenotenrolledcourses (Optional[int], optional): whether to return courses that the user can see even if is not enroled in. This requires the parameter courseids to not be empty. Defaults to None.
+
+        Returns:
+            Assignments: Returns the courses and assignments for the users capability
+        """
         res = self.moodle.post(
             'mod_assign_get_assignments',
             courseids=courseids,
@@ -30,6 +40,15 @@ class BaseAssign(BaseMoodle):
     def get_grades(self,
                    assignmentids: List[int],
                    since: Union[datetime, int] = 0) -> Grades:
+        """Returns grades from the assignment
+
+        Args:
+            assignmentids (List[int]): 1 or more assignment ids
+            since (Union[datetime, int], optional):  timestamp, only return records where timemodified >= since. Defaults to 0.
+
+        Returns:
+            Grades: Returns grades from the assignment
+        """
         res = self.moodle.post(
             'mod_assign_get_grades',
             assignmentids=assignmentids,
@@ -47,6 +66,21 @@ class BaseAssign(BaseMoodle):
             onlyids: Optional[int] = None,
             includeenrolments: int = 1,
             tablesort: Optional[int] = None) -> List[Participant]:
+        """List the participants for a single assignment, with some summary info about their submissions.
+
+        Args:
+            assignid (int): assign instance id
+            groupid (int): group id
+            filter (str): search string to filter the results
+            skip (int, optional): number of records to skip. Defaults to 0.
+            limit (int, optional): maximum number of records to return. Defaults to 0.
+            onlyids (Optional[int], optional): Do not return all user fields. Defaults to None.
+            includeenrolments (int, optional): Do return courses where the user is enrolled. Defaults to 1.
+            tablesort (Optional[int], optional): Apply current user table sorting preferences.. Defaults to None.
+
+        Returns:
+            List[Participant]: list of participant
+        """
         res = self.moodle.post(
             'mod_assign_list_participants',
             assignid=assignid,
@@ -142,6 +176,15 @@ class BaseAssign(BaseMoodle):
 
     def submit_for_grading(self, assignmentid: int,
                            acceptsubmissionstatement: int) -> List[Warning]:
+        """Submit the current students assignment for grading
+
+        Args:
+            assignmentid (int): The assignment id to operate on
+            acceptsubmissionstatement (int): Accept the assignment submission statement
+
+        Returns:
+            List[Warning]: list of warnings
+        """
         res = self.moodle.post(
             'mod_assign_submit_for_grading',
             assignmentid=assignmentid,
@@ -150,6 +193,16 @@ class BaseAssign(BaseMoodle):
 
     def submit_grading_form(self, assignmentid: int, userid: int,
                             jsonformdata: str) -> List[Warning]:
+        """Submit the grading form data via ajax
+
+        Args:
+            assignmentid (int): The assignment id to operate on
+            userid (int): The user id the submission belongs to
+            jsonformdata (str): The data from the grading form, encoded as a json array
+
+        Returns:
+            List[Warning]: list of warnings
+        """
         res = self.moodle.post('mod_assign_submit_grading_form')
         return [from_dict(Warning, data) for data in res] if res else []
 
