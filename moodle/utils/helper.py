89ae127f4eec12b16334cb57efdff9e143fc86fb
hexatester <revolusi147id@gmail.com>
2020-09-17 22:14:09 +0700

    implement qs list format to indices

--- moodle/utils/helper.py
+++ moodle/utils/helper.py
@@ -3,6 +3,7 @@ from dacite.data import Data
 from dataclasses import is_dataclass, asdict
 from datetime import datetime
 from typing import Type, TypeVar, Any
+
 from moodle.config import DACITE_CONFIG
 
 T = TypeVar("T")
@@ -22,15 +23,26 @@ def from_dict(data_class: Type[T], data: Data) -> T:
     return dacite_from_dict(data_class, data, config=DACITE_CONFIG)
 
 
-def to_dict(data: Any) -> Any:
+def to_dict(data: Any, name: str = '') -> Any:
     if not data:
         return data
     if isinstance(data, list):
-        return [to_dict(d) for d in data]
+        out = {}
+        for idx, val in enumerate(data):
+            val = to_dict(val)
+            if isinstance(val, dict):
+                for key, value in val.items():
+                    out[f'{name}[{idx}][{key}]'] = val
+            else:
+                out[f'{name}[{getattr(data, "name", "")}][{idx}]'] = val
+        return out
     if isinstance(data, dict):
         out = {}
         for key, value in data.items():
-            out[key] = to_dict(value)
+            if isinstance(value, list):
+                out.update(to_dict(value, key))
+            else:
+                out[key] = value
         return out
     if is_dataclass(data):
         return asdict(data)
