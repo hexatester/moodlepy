0e71bad8e53a1292e583510e2d4c741801626468
hexatester <revolusi147id@gmail.com>
2020-09-22 21:21:59 +0700

    register  Lesson, OneLesson, Lessons to mod_lesson

--- moodle/mod/lesson/__init__.py
+++ moodle/mod/lesson/__init__.py
@@ -1,3 +1,9 @@
+from .lesson import (
+    Lesson,
+    OneLesson,
+    Lessons,
+)
+
 from .base import BaseLesson
 
-__all__ = ['BaseLesson']
+__all__ = ['Lesson', 'OneLesson', 'Lessons', 'BaseLesson']
