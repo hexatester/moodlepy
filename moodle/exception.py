13d9f22d811574ec7765f3a5af5e0a8470031211
hexatester <revolusi147id@gmail.com>
2020-10-26 18:17:38 +0700

    add NetworkMoodleException EmptyResponseException

--- moodle/exception.py
+++ moodle/exception.py
@@ -1,28 +1,41 @@
 from dataclasses import dataclass
-from typing import Optional
+from typing import Any, Optional
+from requests.exceptions import RequestException
 
 
 @dataclass
 class MoodleException(Exception):
-    errorcode: Optional[str] = ''
-    exception: Optional[str] = ''
+    errorcode: Optional[Any] = ''
+    exception: Optional[Any] = ''
     message: Optional[str] = ''
 
     def __str__(self):
         return self.message or self.exception or self.errorcode
 
 
-@dataclass
 class BaseException(Exception):
-    msg: str = ''
+    message: str = ''
 
     def __post_init__(self) -> None:
-        self.msg = self.msg.capitalize()
+        self.message = self.message.capitalize()
 
     def __str__(self) -> str:
-        return self.msg
+        return self.message
 
 
 @dataclass
-class InvalidCredentialException(Exception):
-    msg: str = 'Wrong username or password!'
+class EmptyResponseException(BaseException):
+    message: str = 'Empty response from server!'
+
+
+@dataclass
+class InvalidCredentialException(BaseException):
+    message: str = 'Wrong username or password!'
+
+
+@dataclass
+class NetworkMoodleException(BaseException):
+    """Moodle wrapper for network related network error
+    """
+    exception: Optional[RequestException] = None
+    message: str = 'A Network error occurred'
