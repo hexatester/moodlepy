6efe53376cef55522e5b29e2bb3afd112bc9e647
hexatester <revolusi147id@gmail.com>
2020-10-27 22:03:59 +0700

    add deprecated decorator

--- moodle/utils/decorator.py
+++ moodle/utils/decorator.py
@@ -1,11 +1,31 @@
+import logging
+from functools import wraps
 from typing import Callable, Type
 from moodle import MoodleObject
 
 
 def meta(wsfunction: str, moodlewsrestformat: str) -> Callable:
     def decorator(cls: Type[MoodleObject]) -> Type[MoodleObject]:
-        cls.wsfunction = wsfunction
-        cls.moodlewsrestformat = moodlewsrestformat
+        setattr(cls, 'wsfunction', wsfunction)
+        setattr(cls, 'moodlewsrestformat', moodlewsrestformat)
         return cls
 
     return decorator
+
+
+def deprecated(func: Callable):
+    """Specify the method is deprecated
+
+    Args:
+        func (Callable): Wsfunction of moodlepy
+
+    Returns:
+        Callable: Decorated func
+    """
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        logging.warning(
+            '** DEPRECATED ** Please do not call this function any more')
+        return func(*args, **kwargs)
+
+    return wrapper
