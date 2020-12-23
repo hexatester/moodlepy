9dea673af2632ca5cebe41f93741d1a32f4781b2
hexatester <revolusi147id@gmail.com>
2020-12-23 08:30:43 +0700

    update UserCustomField

--- moodle/core/user/user_custom_field.py
+++ moodle/core/user/user_custom_field.py
@@ -1,11 +1,18 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
 from moodle.base.general import GeneralTypeValue
 
 
+@dataclass
 class UserCustomField(GeneralTypeValue):
     """User custom fields (also known as user profil fields)
 
     Args:
         type (str): The name of the custom field
         value (str): The value of the custom field
+        name (Optional[str]): The name of the custom field
+        shortname (Optional[str]): The shortname of the custom field - to be able to build the field class in the code
     """
-    pass
+    name: Optional[str] = field(default=None)
+    shortname: Optional[str] = field(default=None)
