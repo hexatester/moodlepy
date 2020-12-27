a11bb4d55f63ea61d47cc3b45c18a14aaf197699
hexatester <revolusi147id@gmail.com>
2020-12-27 12:30:59 +0700

    fix enrol_guest_get_instance_info

--- moodle/enrol/guest/base.py
+++ moodle/enrol/guest/base.py
@@ -5,5 +5,8 @@ from . import InstanceInfoResponse
 
 class BaseGuest(BaseMoodle):
     def get_instance_info(self, instanceid: int) -> InstanceInfoResponse:
-        data = self.moodle.post('enrol_guest_get_instance_info')
+        data = self.moodle.post(
+            'enrol_guest_get_instance_info',
+            instanceid=instanceid,
+        )
         return from_dict(InstanceInfoResponse, data)
