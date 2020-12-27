2956d40637b553f3fa6391a7947eabcdc368da9c
hexatester <revolusi147id@gmail.com>
2020-12-27 12:53:45 +0700

    v0.16.0 - rename Warning, and more functions added

--- tests/test_moodle.py
+++ tests/test_moodle.py
@@ -2,6 +2,7 @@ from moodle import (
     __version__,
     Auth,
     Core,
+    Enrol,
     Mod,
     Tool,
     Moodle,
@@ -9,12 +10,13 @@ from moodle import (
 
 
 def test_version():
-    assert __version__ == '0.15.0'
+    assert __version__ == '0.16.0'
 
 
 def test_moodle(moodle: Moodle):
     assert isinstance(moodle, Moodle)
     assert isinstance(moodle.auth, Auth)
     assert isinstance(moodle.core, Core)
+    assert isinstance(moodle.enrol, Enrol)
     assert isinstance(moodle.mod, Mod)
     assert isinstance(moodle.tool, Tool)
