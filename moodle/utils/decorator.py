41e5367be47fb6c3b5ae48edbfe1011c68784f2f
hexatester <revolusi147id@gmail.com>
2021-01-05 20:28:28 +0700

    remove unused decorator

--- moodle/utils/decorator.py
+++ moodle/utils/decorator.py
@@ -1,16 +1,6 @@
 import logging
 from functools import wraps
-from typing import Callable, Type
-from moodle import MoodleObject
-
-
-def meta(wsfunction: str, moodlewsrestformat: str) -> Callable:
-    def decorator(cls: Type[MoodleObject]) -> Type[MoodleObject]:
-        setattr(cls, 'wsfunction', wsfunction)
-        setattr(cls, 'moodlewsrestformat', moodlewsrestformat)
-        return cls
-
-    return decorator
+from typing import Callable
 
 
 def deprecated(func: Callable):
