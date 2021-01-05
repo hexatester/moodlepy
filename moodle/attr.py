d89941505b54e878f1a824993b6bd915f9555fa3
hexatester <revolusi147id@gmail.com>
2021-01-05 10:02:58 +0700

    initial .attr for refactor

---
+++ moodle/attr.py
@@ -0,0 +1,73 @@
+from attr import attrs, attrib, has, Attribute
+from attr import asdict as asdict_attr
+from datetime import datetime
+from dataclasses import is_dataclass
+from dataclasses import asdict as asdict_dc
+from functools import partial
+from typing import Any, Callable, List
+
+from moodle.base.moodle_object import MoodleObject  # NOQA
+
+COMMON: List = [str, int, datetime]
+
+
+def hooq(cls: type, fields: List[Attribute]) -> List[Attribute]:
+    results: List[Attribute] = list()
+    return results
+
+
+def serialize(inst=None,
+              field: Attribute = None,
+              value: Any = None,
+              name: str = None) -> Any:
+    if not value:
+        return value
+    if not name:
+        name = getattr(field, 'name', '')
+    if isinstance(value, list):
+        out = {}
+        for idx, val in enumerate(value):
+            val = serialize(value=val)
+            if isinstance(val, dict):
+                for key, value in val.items():
+                    out[f'{name}[{idx}][{key}]'] = val[key]
+            else:
+                out_key = name or ''
+                # Check if data required name prefix
+                if hasattr(value, 'name'):
+                    out_key += f"[{getattr(value, 'name')}]"
+                out_key += f'[{idx}]'
+                out[out_key] = val
+        return out
+    elif isinstance(value, dict):
+        out = {}
+        for key, value in value.items():
+            if isinstance(value, list):
+                out.update(serialize(value=val, name=key))
+            else:
+                out[key] = value
+        return out
+    elif has(value):
+        return asdict_attr(value, value_serializer=serialize)  # type: ignore
+    elif isinstance(value, datetime):
+        return datetime.timestamp(value)
+    elif is_dataclass(value):
+        return asdict_dc(value)
+    return value
+
+
+asdict: Callable = partial(asdict_attr, value_serializer=serialize)
+dataclass: Callable = partial(attrs, auto_attribs=True)
+sdataclass: Callable = partial(attrs, auto_attribs=True, slots=True)
+
+
+def fields(converter: Callable):
+    def conv(d: List[dict]):
+        results: List = list()
+        if not isinstance(d, list):
+            return results
+        for result in results:
+            results.append(converter(**result))
+        return results
+
+    return attrib(converter=conv)
