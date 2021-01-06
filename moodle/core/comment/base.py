c234afe7799e32bc230860c09b8552a5f7ca40d1
hexatester <revolusi147id@gmail.com>
2021-01-06 23:40:16 +0700

    add core_comment_get_comments

--- moodle/core/comment/base.py
+++ moodle/core/comment/base.py
@@ -1,4 +1,5 @@
 from moodle import BaseMoodle
+from . import Comments
 
 
 class BaseComment(BaseMoodle):
@@ -10,6 +11,36 @@ class BaseComment(BaseMoodle):
         data = self.moodle.get('core_comment_delete_comments')
         return data
 
-    def get_comments(self):
-        data = self.moodle.get('core_comment_get_comments')
-        return data
+    def get_comments(self,
+                     contextlevel: str,
+                     instanceid: int,
+                     component: int,
+                     itemid: int,
+                     area: str = "",
+                     page: int = 0,
+                     sortdirection: str = "DESC") -> Comments:
+        """Returns comments.
+
+        Args:
+            contextlevel (str): contextlevel system, course, user...
+            instanceid (int): the Instance id of item associated with the context level
+            component (int): component
+            itemid (int): associated id
+            area (str, optional): string comment area. Defaults to "".
+            page (int, optional): page number (0 based). Defaults to 0.
+            sortdirection (str, optional): Sort direction: ASC or DESC. Defaults to "DESC".
+
+        Returns:
+            Comments: Response
+        """
+        data = self.moodle.get(
+            'core_comment_get_comments',
+            contextlevel=contextlevel,
+            instanceid=instanceid,
+            component=component,
+            itemid=itemid,
+            area=area,
+            page=page,
+            sortdirection=sortdirection,
+        )
+        return self._tr(Comments, data)
