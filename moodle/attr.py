eb9d1a0e667332c8789425aa95438c2ba88a48e6
hexatester <revolusi147id@gmail.com>
2021-04-06 00:59:13 +0700

    Remove unused stuff

--- moodle/attr.py
+++ moodle/attr.py
@@ -2,24 +2,9 @@ from attr import attrs, attrib, has, Attribute
 from attr import asdict as asdict_attr
 from datetime import datetime
 from functools import partial
-from typing import Any, Callable, List, Optional
-
-from moodle.base.moodle_object import MoodleObject  # NOQA
+from typing import Any, Callable, List
 
 COMMON: List = [str, int, datetime]
-
-
-def fromtimestamp(d: str):
-    if not isinstance(d, str):
-        return d
-    elif not d.isdigit():
-        return d
-    return datetime.fromtimestamp(float(d))
-
-
-def hooq(cls: type, fields: List[Attribute]) -> List[Attribute]:
-    results: List[Attribute] = list()
-    return results
 
 
 def serialize(inst=None,
@@ -60,24 +45,6 @@ def serialize(inst=None,
     return value
 
 
-def auto_convert(cls, fields):
-    results = []
-    for field in fields:
-        if field.converter is not None:
-            results.append(field)
-            continue
-        if field.type in {datetime, 'datetime'}:
-            converter = fromtimestamp
-        elif field.type == Optional[field.type]:
-
-            def converter(d=None):
-                return d
-        else:
-            converter = None
-        results.append(field.evolve(converter=converter))
-    return results
-
-
 asdict: Callable = partial(
     asdict_attr,
     value_serializer=serialize,
@@ -85,12 +52,5 @@ asdict: Callable = partial(
 dataclass: Callable = partial(
     attrs,
     auto_attribs=True,
-    # field_transformer=auto_convert,
-)
-sdataclass: Callable = partial(
-    attrs,
-    auto_attribs=True,
-    field_transformer=auto_convert,
-    slots=True,
 )
 field = attrib
