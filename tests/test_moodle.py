fbfc3f22e63f46938302f06e11e396df983b3726
hexatester <revolusi147id@gmail.com>
2021-07-28 23:40:16 +0700

    Update test_moodle

--- tests/test_moodle.py
+++ tests/test_moodle.py
@@ -3,6 +3,7 @@ from moodle import (
     Auth,
     Core,
     Enrol,
+    Mdl,
     Mod,
     Tool,
     Moodle,
@@ -15,6 +16,7 @@ def test_version():
 
 
 def test_moodle(moodle: Moodle):
+    assert issubclass(Moodle, Mdl)
     assert isinstance(moodle, Moodle)
     assert isinstance(moodle.auth, Auth)
     assert isinstance(moodle.core, Core)
